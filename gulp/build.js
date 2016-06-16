34188d32024284fe984faf647a53c7f320dea24e
jedmundo <jedmundo@mobiquityinc.com>
2016-06-16 14:37:15 +0200

    small fix

--- gulp/build.js
+++ gulp/build.js
@@ -57,7 +57,7 @@ gulp.task('other', function () {
 
     return gulp.src([
             path.join(conf.paths.src, '/**/*'),
-            path.join('!' + conf.paths.src, '/**/*.{html,css,js,less}')
+            path.join('!' + conf.paths.src, '/**/*.{html,css,js,scss}')
         ])
         .pipe(fileFilter)
         .pipe(gulp.dest(path.join(conf.paths.dist, '/')));
